1969ac24586938153a6ae759014ddcc458ba626a
sandhyasawalkar1999 <sandhyasawalkar1999@gmail.com>
2025-03-22 18:02:13 +0530

    code

--- admin/src/Pages/Add/Add.jsx
+++ admin/src/Pages/Add/Add.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 import { toast } from 'react-toastify'
 const Add = ({ url }) => {
 
-  const url = 'http://localhost:4000'
+  const url = 'https://delivery-app-wxc1.onrender.com'
   const [image, setImage] = useState(null);
   const [data, setData] = useState({
     name: '',
